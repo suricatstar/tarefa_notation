be171f70c5a2c8562286377b1be7063d189dbb4e
Suricatstar <caue.tamiarana@gmail.com>
2025-12-12 10:51:23 -0300

    finalizada a atividade.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("\n sua tabela cliente está assim: ");
+
+        Cliente cliente = new Cliente();
+
+        cliente.setId(1L);
+        cliente.setNome("João da Silva");
+
+        System.out.printf("\n O Cliente %s foi cadastrado com o ID %d %n", cliente.getNome(), cliente.getId());
+
+        Boolean hasNameTabela = cliente.getClass().isAnnotationPresent(annotation.Tabela.class);
+
+        String nomeTabela = cliente.getClass().getAnnotation(annotation.Tabela.class).value();
+
+        System.out.println("\n e a Tabela tem nome? " + hasNameTabela);
+        System.out.println("\n Nome da Tabela: " + nomeTabela);
     }
 }
